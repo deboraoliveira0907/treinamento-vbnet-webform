6e1c12b271891bc0ca72268e4ff4e6814cb8d0ff
Debora Alves de Oliveira <debora@dcbtor.com.br>
2020-09-17 16:52:12 -0400

    Arquivo atualizado teste

--- sql/Script_Criacao_Banco_Dados.sql
+++ sql/Script_Criacao_Banco_Dados.sql
@@ -1,13 +1,4 @@
 
-
-
-IF(SELECT COUNT(*) FROM sys.tables t
-where t.name = 'Aluno') > 0 
-BEGIN
-	DROP TABLE Aluno
-END
-GO
-
 CREATE TABLE [dbo].[Aluno](
 	[IdAluno] [int] NOT NULL PRIMARY KEY,
 	[NomeAluno] [varchar](100) NOT NULL,
